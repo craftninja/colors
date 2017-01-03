c9e840a356b9c6100d04e4f67e548998d9c858fe
Emily Platzer <emilyplatzer@gmail.com>
2017-01-02 18:51:28 -0700

    refactor - DOM updates are now in single function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,34 +8,44 @@ window.onload = () => {
 
   let redDrops = 0, yellowDrops = 0, blueDrops = 0;
 
+  function isDisabled(element) {
+    return element.className.includes('disabled')
+  }
+
+  function updateColors () {
+    document.getElementById("totalRed").innerHTML = redDrops
+    document.getElementById("totalYellow").innerHTML = yellowDrops
+    document.getElementById("totalBlue").innerHTML = blueDrops
+    if (redDrops == 1 && isDisabled(removeRed)) removeRed.className = removeRed.className.split('disabled').join('')
+    if (yellowDrops == 1 && isDisabled(removeYellow)) removeYellow.className = removeYellow.className.split('disabled').join('')
+    if (blueDrops == 1 && isDisabled(removeBlue)) removeBlue.className = removeBlue.className.split('disabled').join('')
+    if (redDrops == 0 && !isDisabled(removeRed)) removeRed.className = removeRed.className + (' disabled')
+    if (yellowDrops == 0 && !isDisabled(removeYellow)) removeYellow.className = removeYellow.className + (' disabled')
+    if (blueDrops == 0 && !isDisabled(removeBlue)) removeBlue.className = removeBlue.className + (' disabled')
+  }
+
   addRed.onclick = () => {
     redDrops += 1
-    document.getElementById("totalRed").innerHTML = redDrops
-    if (redDrops == 1) removeRed.className = removeRed.className.split('disabled').join('')
+    updateColors()
   };
   removeRed.onclick = () => {
     if (redDrops > 0) redDrops -= 1
-    document.getElementById("totalRed").innerHTML = redDrops
-    if (redDrops == 0) removeRed.className = removeRed.className + (' disabled')
+    updateColors()
   };
   addYellow.onclick = () => {
     yellowDrops += 1
-    document.getElementById("totalYellow").innerHTML = yellowDrops
-    if (yellowDrops == 1) removeYellow.className = removeYellow.className.split('disabled').join('')
+    updateColors()
   };
   removeYellow.onclick = () => {
     if (yellowDrops > 0) yellowDrops -= 1
-    document.getElementById("totalYellow").innerHTML = yellowDrops
-    if (yellowDrops == 0) removeYellow.className = removeYellow.className + (' disabled')
+    updateColors()
   };
   addBlue.onclick = () => {
     blueDrops += 1
-    document.getElementById("totalBlue").innerHTML = blueDrops
-    if (blueDrops == 1) removeBlue.className = removeBlue.className.split('disabled').join('')
+    updateColors()
   };
   removeBlue.onclick = () => {
     if (blueDrops > 0) blueDrops -= 1
-    document.getElementById("totalBlue").innerHTML = blueDrops
-    if (blueDrops == 0) removeBlue.className = removeBlue.className + (' disabled')
+    updateColors()
   };
 }
